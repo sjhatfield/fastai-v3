39ca95be1faf2b1a4e2efc0cc93333d14d7e081c
sjhatfield <s.j.hatfield@live.co.uk>
2020-05-26 20:29:43 +0100

    added confidence to javascript

--- app/static/client.js
+++ app/static/client.js
@@ -7,7 +7,7 @@ function showPicker() {
 function showPicked(input) {
   el("upload-label").innerHTML = input.files[0].name;
   var reader = new FileReader();
-  reader.onload = function(e) {
+  reader.onload = function (e) {
     el("image-picked").src = e.target.result;
     el("image-picked").className = "";
   };
@@ -23,13 +23,14 @@ function analyze() {
   var loc = window.location;
   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
     true);
-  xhr.onerror = function() {
+  xhr.onerror = function () {
     alert(xhr.responseText);
   };
-  xhr.onload = function(e) {
+  xhr.onload = function (e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
       el("result-label").innerHTML = `Result = ${response["result"]}`;
+      el("confidence-label").innerHTML = `Confidence = ${response["result"]} (decimal percentage)`;
     }
     el("analyze-button").innerHTML = "Analyze";
   };

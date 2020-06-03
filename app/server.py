197bdc5894a730dbd79d5160b609050f17c778c0
sjhatfield <s.j.hatfield@live.co.uk>
2020-06-03 12:28:34 +0100

    typo predicition

--- app/server.py
+++ app/server.py
@@ -111,7 +111,7 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data["file"].read())
     img = open_image(BytesIO(img_bytes))
-    predicition = learn.predict(img)[0]
+    prediction = learn.predict(img)[0]
     return JSONResponse({"result": str(prediction)})
 
 
